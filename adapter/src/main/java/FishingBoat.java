f5f332ae873527bb99a92230404eb38f8383f4c7
mingjiexian <mingjiexian@thunisoft.com>
2019-08-25 21:35:49 +0800

    add adapter

---
+++ adapter/src/main/java/FishingBoat.java
@@ -0,0 +1,13 @@
+/**
+ * @ClassName FishingBoat
+ * @Description TODO
+ * @Author mingjie
+ * @Date 2019/8/25 21:33
+ * @Versrion 1.0
+ **/
+public class FishingBoat {
+
+    public void sail(){
+        System.out.println("The fishing boat is sailing");
+    }
+}
